78b4e31f6af7af3083a96f5c5fcfabea5806e8e3
tillepille <16536696+tillepille@users.noreply.github.com>
2019-01-27 11:28:11 +0100

    cleanup

--- Emitter.js
+++ Emitter.js
@@ -1,21 +1,14 @@
-const _ = require('underscore');
-const Q = require('q');
-const path = require('path');
-const exec = require('child_process').exec;
-const util = require('util');
-
-
+const _ = require('underscore')
+const Q = require('q')
+const path = require('path')
+const exec = require('child_process').exec
 
 module.exports = Emitter;
 
-
-
-Emitter.SCRIPT = 'build/codesend';
+Emitter.SCRIPT = 'build/codesend'
 
 function Emitter(options) {
-
-    this.options = options;
-
+    this.opts = options
 }
 
 /**
@@ -29,86 +22,58 @@ function Emitter(options) {
  * @return  Promise
  */
 Emitter.prototype.sendCode = function(code, options, callback) {
-
-    var deferred = Q.defer();
-
-    //NoOp as default callback
-    if (!_.isFunction(callback)) {
-        callback = _.noop;
-    }
+    var deferred = Q.defer()
 
     //Check arguments length
     if (arguments.length === 0 || arguments.length > 3) {
-        return deferred.reject(new Error('Invalid parameters. sendCode(code, [options, callback])'));
+        return deferred.reject(new Error('Invalid parameters. sendCode(code, [options, callback])'))
     }
 
     //Check if code is a number (and parse it)
-    code = parseInt(code);
-    if (!_.isNumber(code)) {
-        return deferred.reject(new Error('First parameter must be a integer'));
+    code = parseInt(code)
+    if (typeof(code) !== 'number'){
+        return deferred.reject(new Error('First parameter must be a integer'))
     }
 
-    //Tidy up
     switch (arguments.length) {
-
         //function(code)
         case 1:
-
             options = this.options;
+            break
 
-            break;
-
-            //function(code, options || callback)
+            //  function(code, options || callback)
         case 2:
+            //  function(code, callback)
+            if (typeof(options) === 'function') {
+                callback = options
+                options = this.options
 
-            //function(code, callback)
-            if (_.isFunction(options)) {
-
-                callback = options;
-                options = this.options;
-
-                //function(code, options)
-            } else if (_.isObject(options)) {
-
-                _.defaults(options, this.options);
-
-                //function(code, ???)
-            } else {
-
-                return deferred.reject(new Error('Second parameter must be a function (callback) or an object (options)'));
-
+            //  function(code, ???)
+            } else if (typeof(options) !== 'object') {
+                return deferred.reject(new Error('Second parameter must be a function (callback) or an object (options)'))
             }
-
-            break;
-
-            //function(code, options, callback)
+            break
+            //  function(code, options, callback)
         default:
-
-            _.defaults(options, this.options);
-
-            break;
-
+            if(!options){
+                options = this.opts
+            }
+            break
     }
 
-    //Send the code
+    //  Send the code
     exec([path.join(__dirname, Emitter.SCRIPT),
         '--code', code,
         '--pin', options.pin,
         '--pulse-length', options.pulseLength
     ].join(' '), function(error, stdout, stderr) {
-
-        error = error || stderr;
-
+        error = error || stderr
         if (error) {
-            deferred.reject(error);
+            deferred.reject(error)
         } else {
-            deferred.resolve(stdout);
+            deferred.resolve(stdout)
         }
-
-        callback(error, stdout);
-
-    });
-
+        callback(error, stdout)
+    })
     return deferred.promise;
-
-};
+}
